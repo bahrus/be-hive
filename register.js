3493bba9ea94ff10703c53c9e774120f52da988a
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-11-24 07:42:23 -0500

    0.0.11

--- register.js
+++ register.js
@@ -1,16 +1,14 @@
 import('./be-hive.js');
 export function register(ifWantsToBe, upgrade, extTagName) {
     const beHive = document.querySelector('be-hive');
-    if (beHive !== null) {
-        customElements.whenDefined('be-hive').then(() => {
-            beHive.register({
-                ifWantsToBe,
-                upgrade,
-                localName: extTagName,
-            });
+    if (beHive === null) {
+        document.body.appendChild(document.createElement('be-hive'));
+    }
+    customElements.whenDefined('be-hive').then(() => {
+        beHive.register({
+            ifWantsToBe,
+            upgrade,
+            localName: extTagName,
         });
-    }
-    else {
-        document.head.appendChild(document.createElement(extTagName));
-    }
+    });
 }
--- register.ts
+++ register.ts
@@ -2,15 +2,15 @@ import {BeHiveActions} from './types';
 import('./be-hive.js');
 export function register(ifWantsToBe: string, upgrade: string, extTagName: string){
     const beHive = document.querySelector('be-hive') as Element & BeHiveActions;
-    if(beHive !== null){
-        customElements.whenDefined('be-hive').then(() => {
-            beHive.register({
-                ifWantsToBe,
-                upgrade,
-                localName: extTagName,
-            })
-        })
-    }else{
-        document.head.appendChild(document.createElement(extTagName));
+    if(beHive===null){
+        document.body.appendChild(document.createElement('be-hive'));
     }
+    customElements.whenDefined('be-hive').then(() => {
+        beHive.register({
+            ifWantsToBe,
+            upgrade,
+            localName: extTagName,
+        })
+    });
+    
 }
