852123b5159d99d39961113dd6b2104bcfad070f
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-07-02 08:12:11 -0400

    0.0.46

---
+++ beatify.js
@@ -0,0 +1,20 @@
+export function beatify(content, beHive) {
+    const decoratorElements = Array.from(beHive.children);
+    for (const decorEl of decoratorElements) {
+        const ifWantsToBe = decorEl.getAttribute('if-wants-to-be');
+        if (ifWantsToBe === undefined)
+            continue;
+        const isAttr = 'is-' + ifWantsToBe;
+        const beAttr = 'be-' + ifWantsToBe;
+        const converted = Array.from(content.querySelectorAll(`[${isAttr}]`));
+        for (const el of converted) {
+            const attr = el.getAttribute(isAttr);
+            el.removeAttribute(isAttr);
+            el.setAttribute(beAttr, attr);
+        }
+    }
+}
+export function beBeatified(element) {
+    const beHive = element.getRootNode().querySelector('be-hive');
+    beatify(element, beHive);
+}
---
+++ beatify.ts
@@ -0,0 +1,20 @@
+export function beatify(content: DocumentFragment | Element, beHive: Element){
+    const decoratorElements = Array.from(beHive.children) as any;
+    for(const decorEl of decoratorElements){
+        const ifWantsToBe = (decorEl as any as Element).getAttribute('if-wants-to-be');
+        if(ifWantsToBe === undefined) continue;
+        const isAttr = 'is-' + ifWantsToBe;
+        const beAttr = 'be-' + ifWantsToBe;
+        const converted = Array.from(content.querySelectorAll(`[${isAttr}]`));
+        for(const el of converted){
+            const attr = el.getAttribute(isAttr)!;
+            el.removeAttribute(isAttr);
+            el.setAttribute(beAttr, attr);
+        }
+    }
+}
+
+export function beBeatified(element: Element){
+    const beHive = (element.getRootNode() as ShadowRoot).querySelector('be-hive') as Element;
+    beatify(element, beHive);    
+}
