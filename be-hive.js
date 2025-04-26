a8aace8ad13e74825cc20838e244e4d361b5af9d
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-04-26 09:58:38 -0400

    0.0.254

--- be-hive.js
+++ be-hive.js
@@ -1,4 +1,5 @@
 import { Synthesizer } from 'mount-observer/Synthesizer.js';
+export { MountObserver } from 'mount-observer/MountObserver.js';
 import 'be-enhanced/beEnhanced.js';
 import { Enhancers } from 'be-enhanced/beEnhanced.js';
 import { assignGingerly } from 'trans-render/lib/assignGingerly.js';
