b9f8ec3f093c83fbebfbf1a5e0d889f51ddf8504
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-07-14 07:34:25 -0400

    0.0.261

--- whenResolved.js
+++ whenResolved.js
@@ -1,6 +1,6 @@
 export async function whenResolved(enhancedElement, base) {
     const rn = enhancedElement.getRootNode();
-    const mose = rn.getElementById(`be-hive-${base}`);
+    const mose = rn.getElementById(`be-hive.${base}`);
     if (mose === null)
         throw 404;
     const emc = mose.synConfig;
--- whenResolved.ts
+++ whenResolved.ts
@@ -1,6 +1,6 @@
 export async function whenResolved(enhancedElement: Element, base: string){
     const rn = enhancedElement.getRootNode() as DocumentFragment;
-    const mose = rn.getElementById(`be-hive-${base}`);
+    const mose = rn.getElementById(`be-hive.${base}`);
     if(mose === null) throw 404;
     const emc = (<any>mose).synConfig;
     return await (<any>enhancedElement).beEnhanced.whenResolved(emc);
