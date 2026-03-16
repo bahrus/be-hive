ae2f108cd94d492847f9c0e0b42f2befbf641825
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2026-03-16 07:41:28 -0400

    0.1.3

--- be-hive.ts
+++ be-hive.ts
@@ -1,5 +1,5 @@
 import {MOSE} from 'mount-observer-script-element/MOSE.js';
-import  'mount-observer/index.js'
+import 'mount-observer/handlers/EnhanceMountedElement.js';
 
 export class BeHive extends MOSE(HTMLElement){
 
